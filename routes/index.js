3044330967f786af1e3b81ddcd1c7fa35a4d829d
Simran Khosla <email.simrankhosla@gmail.com>
2016-01-06 10:39:04 -0600

    sending up some adjustments and views

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/welcome');
+  res.render('pages/welcome', { user: req.user });
 });
 
-router.get('/deck/:deck', function(req, res, next) {
+router.get('/deck', function(req, res, next) {
   var currentDeck = req.params.deck;
-  res.render('pages/deckview');
-  res.render('pages/welcome', { user: req.user });
+  res.render('pages/deckview', { user: req.user });
 });
 
 
